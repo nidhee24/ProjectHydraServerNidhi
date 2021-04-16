ce66d4bf49e1a076019fbf409b1afb085305f4dc
nidhee24 <60859490+nidhee24@users.noreply.github.com>
2021-04-16 13:24:23 -0400

    Nidhi's final heroku

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.use('/api/about',aboutRoute);
 app.use('/api/deal',dealRoutes);
 app.use('/api/hotelroom',hotelroomRoute);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log('Server started');
 });
